5d0632834b6d5b11761f9f42493700e1646dc461
Nitesh Kumar <niteshstudy007@gmail.com>
2023-06-14 11:47:42 +0530

    modified controllers/user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,31 +1,37 @@
 import userModeler from "../models/user.js"
-import {generateToken} from '../services/authentication.js'
+import { generateToken } from '../services/authentication.js'
 
-const loginHandler = async(req,res)=>{
-
-    const {email,password} = req.body 
-    try {
-        const fetchUserIfPasswordVerified = await userModeler.passwordVerifier(email,password)
-        const token = await generateToken(fetchUserIfPasswordVerified.toObject())
-        return res.cookie("token",token).redirect(`/blog/all`)
-    } catch (error) {
-        return res.send(error.message)
+const loginHandler = async (req, res) => {
+    const receivedToken = req.cookies["token"]
+    if (!receivedToken) {
+        const { email, password } = req.body
+        try {
+            const fetchUserIfPasswordVerified = await userModeler.passwordVerifier(email, password)
+            const token = await generateToken(fetchUserIfPasswordVerified.toObject())
+            return res.cookie("token", token).redirect(`/blog/all`)
+        } catch (error) {
+            return res.send(error.message)
+        }
+    }else{
+        return res.redirect(`/blog/all`)
     }
 }
-const signUpHandler = async(req,res)=>{
+
+const signUpHandler = async (req, res) => {
     try {
-        const {email,fullName,password} = req.body
-        if(req.file){
+        const { email, fullName, password } = req.body
+        const data = { email, fullName, password }
+        if (req.file) {
             const normalizedFilePath = req.file.path.replace(/\\/g, '/');
-            await userModeler.create({email,fullName,password,profileImageUrl:normalizedFilePath})
-        }else{
-            await userModeler.create({email,fullName,password})
-        }  
+            await userModeler.create({ ...data, profileImageUrl: normalizedFilePath })
+        } else {
+            await userModeler.create(data)
+        }
         return res.redirect('/')
     } catch (error) {
         console.log(error.message)
         return res.redirect('/user/signup')
     }
-    
+
 }
-export {loginHandler,signUpHandler}
+export { loginHandler, signUpHandler }
